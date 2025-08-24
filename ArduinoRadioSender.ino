7bc53a087db5c2317eb54e0e487413fa058db520
Polonez <antocie567@gmail.com>
2025-08-24 05:30:21 -0700

    Analog pin function

--- ArduinoRadioSender.ino
+++ ArduinoRadioSender.ino
@@ -5,8 +5,6 @@ int cnt = 0;
 int des = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Welcome");
   // put your setup code here, to run once:
   TCCR2A = (1<<COM2A0) | (1<<WGM21) | (1<<WGM20); //Setup pwm, toggle oc2a when compare match
   TCCR2B = (1<<CS00) | (1<<WGM22); //Waveform generation and no prescaling
@@ -20,26 +18,24 @@ void setup() {
   OCR0A = 1;
 
   TCCR0A |= (1 << WGM01); //CTC Mode
-  TCCR0B |= (1 << CS00 | 1 << CS01); //Prescaler 1
+  TCCR0B |= (1 << CS01); //Prescaler 1
   
   TIMSK0 |= (1 << OCIE0A); //Enable interrupt
   sei();
+  pinMode(A0,INPUT);
 }
 
 void loop() {
-    if(Serial.available() > 0)
-    {
-      des = Serial.parseInt();
-      Serial.println("OK");
-    }
 }
 
 ISR(TIMER0_COMPA_vect){
    cnt++;
-   if(cnt > des)
+   if(cnt < analogRead(A0))
    {
-    OCR2A ^= 1;
-    cnt = 0;
+    OCR2A = 1;
+   }else{
+    OCR2A = 0;
    }
+   if(cnt > 8)cnt = 0;
    
 }
